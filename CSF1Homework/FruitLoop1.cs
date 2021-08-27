0f51108d871cbf95ea88ee352cc476bd02298eb1
Mindi Ford <MindiFord@live.com>
2021-08-26 20:29:47 -0500

    basic fruit loop 1

---
+++ CSF1Homework/FruitLoop1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1Homework
+{
+    class FruitLoop1
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Fruit Loop 1 Lab\n");
+
+            string[] fruits = { "apple", "banana", "canteloupe", "dragonfruit", "elderberry", "fig" };
+
+            for (int i = 0; i < fruits.Length; i++)
+            {
+                Console.WriteLine(fruits[i]);
+            }
+
+        }//END MAIN
+    }
+}
