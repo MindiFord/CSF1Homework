74cffffdc00de6fea24fb141e5d9b9b7f44d911d
Mindi Ford <MindiFord@live.com>
2021-08-31 18:27:12 -0500

    Grades partial complete

---
+++ CSF1Homework/Grades.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1Homework
+{
+    class Grades
+    {
+        static void Main(string[] args)
+        {
+
+            #region Instructions
+            /*
+             * Write a program that asks the user for scores (like they would receive on a test) and then displays each score. The program should stop asking for scores and displays the results when the user enters a negative score.
+             */
+            #endregion
+
+            //HOOOOWWWWWW do I capture the different variables??
+
+            int score = 0;
+
+            do
+            {
+
+                Console.Write("Enter a score: ");
+                score = Convert.ToInt32(Console.ReadLine());
+
+            } while (score >= 0);
+
+            Console.WriteLine(score);
+
+        }
+    }
+}
